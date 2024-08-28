06c7f5dbea9d8c3d54b8ffb3f2e3168ee64694b0
Dmitry Matveyev <public@greenfork.me>
2024-08-28 23:42:18 +0500

    Stub table

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,14 @@
 class StocksController < ApplicationController
   def index
+    today = Time.zone.today
+    @range = today.beginning_of_month .. today.end_of_month
+    @table_data = [
+      {
+        product_name: "Tovar",
+        variant_sku: "1020304",
+        warehouse_name: "Koledino",
+        stocks: {}
+      }
+    ]
   end
 end
